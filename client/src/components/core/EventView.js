0c30f1b78d9b5bd8f89bc0ce8fcfa331b1142aef
evgeniia <pe_ace86@outlook.com>
2021-04-12 14:44:31 +0900

    read more button

--- client/src/components/core/EventView.js
+++ client/src/components/core/EventView.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const EventView = (props) => {
+  const [readMore, setReadMore] = useState(false);
+  const setReadWay = () => {
+    setReadMore(!readMore);
+  }
   return (
     <Container className="event-container">
       <Row>
@@ -9,7 +13,12 @@ const EventView = (props) => {
           <img className="img-event" src={`http://localhost:3000/${props.image}`}/>
         </Col>
         <Col>
-          {props.text}
+          <div className={readMore ? 'text-event': 'text-event-min'}>
+            {props.text}
+          </div>
+          <div className="btn-readmore" onClick={setReadWay}>
+            { readMore ? 'read less...' : 'read more...'}
+          </div>
         </Col>
       </Row>
     </Container>
